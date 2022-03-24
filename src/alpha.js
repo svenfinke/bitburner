5f5bc10fac348f07951da1068789bf0eea48a758
Sven Finke <sven.finke@mail.com>
2022-03-24 15:54:57 +0100

    manage additional servers

--- src/alpha.js
+++ src/alpha.js
@@ -1,7 +1,25 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	if (ns.getPurchasedServerCost(1048576) > ns.getServerMoneyAvailable("home")){
-		throw new Error('Not enough money to purchase a server.');
+	let servername = 'privsrv';
+
+	let files = [
+		'advanced_hacking.js',
+		'basic_hack.js',
+		'batch_script.js',
+		'compromise.js',
+		'rush.js',
+		'weaken_and_grow.js',
+		'worm.js'
+	];
+
+	while (ns.getPurchasedServerCost(1048576) <= ns.getServerMoneyAvailable("home")){
+		ns.purchaseServer(servername, 1048576);
 	}
-	ns.purchaseServer('privsrv', 1048576);
+
+	let servernames = ns.scan();
+	for (let target of servernames) {
+		if (target.startsWith(servername)) {
+			await ns.scp(files, target);
+		}
+	}
 }
